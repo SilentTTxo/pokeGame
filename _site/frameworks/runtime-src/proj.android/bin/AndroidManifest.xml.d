7a3b09f604c121bb4ff3fb478d39353a1f3169cd
SilentTTxo <8359251@qq.com>
2016-05-15 01:49:14 +0800

    0.0

---
+++ _site/frameworks/runtime-src/proj.android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\cocos2dxData\poketest\frameworks\runtime-src\proj.android\bin\AndroidManifest.xml : \
+D:\cocos2dxData\poketest\frameworks\runtime-src\proj.android\AndroidManifest.xml \
---
+++ _site/frameworks/runtime-src/proj.android/gen/R.java.d
@@ -0,0 +1,6 @@
+D:\cocos2dxData\poketest\frameworks\runtime-src\proj.android\gen\org\cocos\poketest\R.java \
+ : D:\cocos2dxData\poketest\frameworks\runtime-src\proj.android\res\drawable-hdpi\icon.png \
+D:\cocos2dxData\poketest\frameworks\runtime-src\proj.android\res\drawable-ldpi\icon.png \
+D:\cocos2dxData\poketest\frameworks\runtime-src\proj.android\res\drawable-mdpi\icon.png \
+D:\cocos2dxData\poketest\frameworks\runtime-src\proj.android\res\values\strings.xml \
+D:\cocos2dxData\poketest\frameworks\runtime-src\proj.android\bin\AndroidManifest.xml \
